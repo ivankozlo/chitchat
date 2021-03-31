1b96f9b39ecb631f80aad87366e22746ad415aa0
ivankozlo <ivan.olegovich.kozlov@gmail.com>
2021-03-31 05:02:26 +0300

    added semantic ui library and enabled jquery dropdown

---
+++ app/javascript/custom/init.js
@@ -0,0 +1,4 @@
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown()
+  $('.menu .item').tab()
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,7 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "semantic-ui-sass"
+
+import "custom/init"
